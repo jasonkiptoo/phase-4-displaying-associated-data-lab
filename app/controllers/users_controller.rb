56345b66471fb6e8859227e3bd04fa6fffd5d843
JasonKiptoo <kiptoojason@gmail.com>
2022-11-26 14:07:48 +0300

    Completed assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+
+  def index
+user = User.all
+render json: user
+  end
+
+
+
+  def show
+
+user = User.find_by(id: params[:id])
+render json: user, include: :items
+  end
 end
